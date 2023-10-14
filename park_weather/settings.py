fa17551b3998db208d824c47516644fb6a9b148c
ottobrew <26239254+ottobrew@users.noreply.github.com>
2023-10-14 00:06:41 -0500

    Update settings.py

--- park_weather/settings.py
+++ park_weather/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = os.environ['DEBUG']
 
 # ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-ALLOWED_HOSTS = os.environ['WEBSITE_HOSTNAME']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 
 # Application definition
 
